7bdf97f991c1b263f5ba34d7b6ed8d95728d7a93
Ui <kyh571021@gmail.com>
2024-02-24 16:27:31 +0900

    Refactor: Dao, Initailizer 리펙토링, Service 객체 생성
    1.Dao 클래스들 Repository로 리펙토링
    2.Service 객체 생성

--- src/main/java/kkkw/subrandom/dao/RecipeDao.java
+++
@@ -1,7 +0,0 @@
-package kkkw.subrandom.dao;
-
-import kkkw.subrandom.domain.recipe.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeDao extends JpaRepository<Recipe, Long> {
-}
--- src/main/java/kkkw/subrandom/dao/recipechoice/VegetableDao.java
+++
@@ -1,7 +0,0 @@
-package kkkw.subrandom.dao.recipechoice;
-
-import kkkw.subrandom.domain.recipe.recipechoice.Vegetable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VegetableDao extends JpaRepository<Vegetable, Long> {
-}
